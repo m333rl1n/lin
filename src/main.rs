b9cff82ea09923c4b3620ecb7288106923686a65
Merlin <m333rl1n@gmail.com>
2024-05-14 23:29:50 +0330

    support `/endpoint/${varable}` links

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{error::Error, io::{self, BufRead}};
 use regex::Regex;
 
-static REGEXES: &str=r#"(?:"|'|=)(((?:[a-zA-Z]{1,10}://|//)[^"'/]{1,}\.[a-zA-Z]{2,}[^"']{0,})|((?:/|\.\./|\./)[^"'><,;| *()(%%$^/\\\[\]][^"'><,;|()]{1,})|([a-zA-Z0-9_\-/]{1,}/[a-zA-Z0-9_\-/.]{1,}\.(?:[a-zA-Z]{1,4}|action)(?:[\?|#][^"|']{0,}|))|([a-zA-Z0-9_\-/]{1,}/[a-zA-Z0-9_\-/]{3,}(?:[\?|#][^"|']{0,}|))|([a-zA-Z0-9_\-]{1,}\.(?:php|asp|aspx|jsp|json|action|html|js|txt|xml)(?:[\?|#][^"|']{0,}|)))(?:"|'| )"#;
+static REGEXES: &str=r#"(?:"|'|=|`)(((?:[a-zA-Z]{1,10}://|//)[^"'/]{1,}\.[a-zA-Z]{2,}[^"']{0,})|((?:/|\.\./|\./)[^`"'><,;| *()(%%$^/\\\[\]][^`"'><,;|()]{1,})|([a-zA-Z0-9_\-/]{1,}/[a-zA-Z0-9_\-/.]{1,}\.(?:[a-zA-Z]{1,4}|action)(?:[\?|#][^"|']{0,}|))|([a-zA-Z0-9_\-/]{1,}/[a-zA-Z0-9_\-/]{3,}(?:[\?|#][^"|']{0,}|))|([a-zA-Z0-9_\-]{1,}\.(?:php|asp|aspx|jsp|json|action|html|js|txt|xml)(?:[\?|#][^"|']{0,}|)))(?:"|'| |`)"#;
 // Linkfinder regexes
 
 fn get_links(body: &String, printed: &mut Vec<String>) {
@@ -10,7 +10,7 @@ fn get_links(body: &String, printed: &mut Vec<String>) {
     let result = re.captures_iter(&body);
 
     for m in result{
-        
+
         let lin = m.get(1).map_or("", |v| v.as_str()).trim().to_string();
 
         if !printed.contains(&lin){
